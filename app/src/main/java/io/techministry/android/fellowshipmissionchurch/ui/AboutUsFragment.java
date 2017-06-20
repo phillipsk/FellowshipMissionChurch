9448aaad1d0ea7e68d77653413857a283f289a50
Kevin Phillips <inquiries@kevinphillips.email>
2017-06-20 07:59:18 -0400

    added padding around card view

--- app/src/main/java/io/techministry/android/fellowshipmissionchurch/ui/AboutUsFragment.java
+++ app/src/main/java/io/techministry/android/fellowshipmissionchurch/ui/AboutUsFragment.java
@@ -28,12 +28,15 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 import io.techministry.android.fellowshipmissionchurch.DetailActivity;
 import io.techministry.android.fellowshipmissionchurch.R;
+import io.techministry.android.fellowshipmissionchurch.adapters.CustomRecyclerAdapter;
 import io.techministry.android.fellowshipmissionchurch.data.DummyData;
+import io.techministry.android.fellowshipmissionchurch.model.AboutUsModel;
 
 public class AboutUsFragment extends Fragment {
 
@@ -41,86 +44,110 @@ public class AboutUsFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
         Bundle savedInstanceState) {
-        RecyclerView rv = (RecyclerView) inflater.inflate
-                (R.layout.fragment_about_us, container, false);
-        setupRecyclerView(rv);
+        View rv = inflater.inflate(R.layout.fragment_about_us, container, false);
         return rv;
     }
 
-    private void setupRecyclerView(RecyclerView recyclerView) {
-        recyclerView.setLayoutManager(new LinearLayoutManager(recyclerView.getContext()));
-        recyclerView.setAdapter(Sim);
-        final SimpleStringRecyclerViewAdapter adapter = new SimpleStringRecyclerViewAdapter(Arrays.asList(DummyData.sDummyStrings));
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        RecyclerView rv = (RecyclerView) view.findViewById(R.id.recyclerview);
 
-        recyclerView.setAdapter(adapter);
-        //  getRandomSublist(DummyData.sDummyStrings, 5)));
+
+
+        String[] titles = getActivity().getResources().getStringArray(R.array.models_titles);
+        String[] arraylist = getActivity().getResources().getStringArray(R.array.models_details);
+        ArrayList<AboutUsModel> models = new ArrayList<>();
+        int size = titles.length;
+        for (int i = 0; i< size; i++) {
+            models.add(new AboutUsModel(titles[i], arraylist[i]));
+        }
+
+        setUpRecyclerView(rv, models);
+
     }
 
-  /*  private List<String> getRandomSublist(String[] array, int amount) {
-        ArrayList<String> list = new ArrayList<>(amount);
-        Random random = new Random();
-        while (list.size() < amount) {
-            list.add(array[random.nextInt(array.length)]);
-        }
-        return list;
-    }*/
-
-    public static class SimpleStringRecyclerViewAdapter
-        extends RecyclerView.Adapter<SimpleStringRecyclerViewAdapter.ViewHolder> {
-
-        private List<String> values;
-
-        public static class ViewHolder extends RecyclerView.ViewHolder {
-            String data;
-            public final View view;
-            public final TextView textView;
-
-            public ViewHolder(View view) {
-                super(view);
-                this.view = view;
-                textView = (TextView) view.findViewById(android.R.id.text1);
-            }
-
-            @Override
-            public String toString() {
-                return super.toString() + " '" + textView.getText();
-            }
-        }
-
-        public String getValueAt(int position) {
-            return values.get(position);
-        }
-
-        public SimpleStringRecyclerViewAdapter(List<String> items) {
-            values = items;
-        }
-
-        @Override
-        public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-            View view =
-                LayoutInflater.from(parent.getContext()).inflate(R.layout.list_item, parent, false);
-            return new ViewHolder(view);
-        }
-
-        @Override
-        public void onBindViewHolder(final ViewHolder holder, int position) {
-            holder.data = values.get(position);
-            holder.textView.setText(holder.data);
-
-            holder.view.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    Context context = v.getContext();
-                    Intent intent = new Intent(context, DetailActivity.class);
-                    intent.putExtra(DetailActivity.EXTRA_DATA_ID, holder.data);
-                    context.startActivity(intent);
-                }
-            });
-        }
-
-        @Override
-        public int getItemCount() {
-            return values.size();
-        }
+    private void setUpRecyclerView(RecyclerView rv, ArrayList<AboutUsModel> models) {
+        rv.setLayoutManager(new LinearLayoutManager(getActivity()));
+        CustomRecyclerAdapter adapter = new CustomRecyclerAdapter(models);
+        rv.setAdapter(adapter);
     }
+
+    //
+//    private void setupRecyclerView(RecyclerView recyclerView) {
+//        recyclerView.setLayoutManager(new LinearLayoutManager(recyclerView.getContext()));
+//        recyclerView.setAdapter(Sim);
+//        final SimpleStringRecyclerViewAdapter adapter = new SimpleStringRecyclerViewAdapter(Arrays.asList(DummyData.sDummyStrings));
+//
+//        recyclerView.setAdapter(adapter);
+//        //  getRandomSublist(DummyData.sDummyStrings, 5)));
+//    }
+//
+//  /*  private List<String> getRandomSublist(String[] array, int amount) {
+//        ArrayList<String> list = new ArrayList<>(amount);
+//        Random random = new Random();
+//        while (list.size() < amount) {
+//            list.add(array[random.nextInt(array.length)]);
+//        }
+//        return list;
+//    }*/
+//
+//    public static class SimpleStringRecyclerViewAdapter
+//        extends RecyclerView.Adapter<SimpleStringRecyclerViewAdapter.ViewHolder> {
+//
+//        private List<String> values;
+//
+//        public static class ViewHolder extends RecyclerView.ViewHolder {
+//            String data;
+//            public final View view;
+//            public final TextView textView;
+//
+//            public ViewHolder(View view) {
+//                super(view);
+//                this.view = view;
+//                textView = (TextView) view.findViewById(android.R.id.text1);
+//            }
+//
+//            @Override
+//            public String toString() {
+//                return super.toString() + " '" + textView.getText();
+//            }
+//        }
+//
+//        public String getValueAt(int position) {
+//            return values.get(position);
+//        }
+//
+//        public SimpleStringRecyclerViewAdapter(List<String> items) {
+//            values = items;
+//        }
+//
+//        @Override
+//        public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+//            View view =
+//                LayoutInflater.from(parent.getContext()).inflate(R.layout.list_item, parent, false);
+//            return new ViewHolder(view);
+//        }
+//
+//        @Override
+//        public void onBindViewHolder(final ViewHolder holder, int position) {
+//            holder.data = values.get(position);
+//            holder.textView.setText(holder.data);
+//
+//            holder.view.setOnClickListener(new View.OnClickListener() {
+//                @Override
+//                public void onClick(View v) {
+//                    Context context = v.getContext();
+//                    Intent intent = new Intent(context, DetailActivity.class);
+//                    intent.putExtra(DetailActivity.EXTRA_DATA_ID, holder.data);
+//                    context.startActivity(intent);
+//                }
+//            });
+//        }
+//
+//        @Override
+//        public int getItemCount() {
+//            return values.size();
+//        }
+//    }
 }
