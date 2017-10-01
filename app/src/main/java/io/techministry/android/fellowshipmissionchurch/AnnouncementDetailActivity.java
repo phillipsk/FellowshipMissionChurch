7ac0de4e512575b444906b8a9cb160fd724b3ce5
Kevin Phillips <inquiries@kevinphillips.email>
2017-10-01 08:35:51 -0400

    Initiated refactoring of Butterknife to findViewById

--- app/src/main/java/io/techministry/android/fellowshipmissionchurch/AnnouncementDetailActivity.java
+++ app/src/main/java/io/techministry/android/fellowshipmissionchurch/AnnouncementDetailActivity.java
@@ -8,32 +8,46 @@ import android.support.v7.widget.Toolbar;
 import android.text.Html;
 import android.view.MenuItem;
 import android.view.View;
-import android.webkit.WebView;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.load.engine.DiskCacheStrategy;
 
-import butterknife.BindView;
-import butterknife.ButterKnife;
+//import butterknife.BindView;
+//import butterknife.ButterKnife;
 import io.techministry.android.fellowshipmissionchurch.models.Announcement;
 
 public class AnnouncementDetailActivity extends AppCompatActivity {
 
     Announcement announcement;
-    @BindView(R.id.backdrop) ImageView backdrop;
-    @BindView(R.id.content) TextView content;
-    @BindView(R.id.toolbar) Toolbar toolbar;
-    @BindView(R.id.back_btn) FloatingActionButton back_btn;
+    ImageView backdrop;
+    TextView content;
+    Toolbar toolbar;
+    FloatingActionButton back_btn;
+//    @BindView(R.id.backdrop) ImageView backdrop;
+//    @BindView(R.id.content) TextView content;
+//    @BindView(R.id.toolbar) Toolbar toolbar;
+//    @BindView(R.id.back_btn) FloatingActionButton back_btn;
     Context mContext;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_announcement_detail);
-        ButterKnife.bind(this);
+//        ButterKnife.bind(this);
         mContext = this;
+
+//        Toast.makeText(mContext,"Message", Toast.LENGTH_SHORT).show();
+//        Toast.makeText(mContext, "", Toast.LENGTH_SHORT).show();
+
+        backdrop = (ImageView) findViewById(R.id.backdrop);
+        content = (TextView) findViewById(R.id.content);
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        back_btn = (FloatingActionButton) findViewById(R.id.back_btn);
+
+
 
         announcement = (Announcement)getIntent().getExtras().getSerializable("announcement");
 
